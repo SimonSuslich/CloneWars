5a1b0b5bf5176ce546605aa00810f104e6ef9ec6
SimonSuslich <simonniklas.suslich@elev.ga.ntig.se>
2024-10-21 09:16:24 +0200

    Upload old code and make it work

--- lib/request.rb
+++ lib/request.rb
@@ -1,9 +1,25 @@
-class Request
+# class Request
   
-  attr_reader :method, :resource
+#   attr_reader :method, :resource
+
+#   def initialize(request_string)
+#     @method = :get
+#     @resource = "/"
+#   end
+# end
+
+
+class Request
+
+  attr_reader :method, :resource, :version, :headers, :params
 
   def initialize(request_string)
-    @method = :get
-    @resource = "/"
+      request_lines = request_string.split("\n")
+      first_line, *rest_lines = request_lines 
+      @method, @resource, @version = first_line.split(" ")
+      @headers = rest_lines.map {|x| {x.split(": ")[0] => x.split(": ")[-1]}}.reduce(:merge)
+      @params = {}
+      @method = @method.downcase.to_sym
   end
+
 end
