745826a30e85300008908e05df32e151ccf9a4b4
SimonSuslich <simonniklas.suslich@elev.ga.ntig.se>
2024-10-25 11:37:27 +0200

    works!!!

--- lib/request.rb
+++ lib/request.rb
@@ -6,34 +6,12 @@ class Request
       request_lines = request_string.split("\n")
       first_line, *rest_lines = request_lines 
       @method, @resource, @version = first_line.split(" ")
-      # params_line = rest_lines.pop
-      p rest_lines
-      @headers = rest_lines.map {|line| line.include?":" ? line.split(":", 2).map(&:strip) : nil }.to_h
-      @params = extract_params(rest_lines)
       @method = @method.downcase.to_sym
+      
+      @headers = rest_lines.map {|element| element.include?(":") ? element.split(":", 2).map(&:strip) : nil}.compact.to_h
+      
+      @params = {}
+      request_string[-2..-1] != "\n\n" ? @params = rest_lines[-1].split("&").map {|element| element.include?("=") ? element.split("=", 2).map(&:strip) : nil}.compact.to_h : nil
+      @resource.include?("?") ? @params = @resource.split("?", 2)[-1].split("&").map {|element| element.include?("=") ? element.split("=", 2).map(&:strip) : nil}.compact.to_h : nil
   end
-
-  def extract_params(request)
-    url_params = extract_url_params(request)
-    form_params = extract_form_params(request)
-    url_params.merge(form_params)
-  end
-
-  def extract_url_params(request)
-    query_string = request.split("\n").find { |line| line.include?("?") }
-    return {} unless query_string
-
-    query_string.split("?")[1].split(" ")[0].split("&").map { |param| param.plit("=") }.to_h
-  end
-
-  def extract_form_params(request)
-    body  =request.split("\n\n", 2)[1]
-    return {} unless body
-
-    body.split("&").map { |param| param.split("=") }.to_h
-  end 
-
 end
-
-request_string = File.read('../test/example_requests/get-index.request.txt')
-p Request.new(request_string).headers
--- test/http_parser_spec.rb
+++ test/http_parser_spec.rb
@@ -42,22 +42,22 @@ describe 'Request' do
 
     end
 
-    describe 'Simple post-request' do
+    describe 'Simple get-request' do
     
-        request_file = './test/example_requests/post-login.request.txt'
+        request_file = './test/example_requests/get-examples.request.txt'
 
         it 'parses the http method' do
             request_string = File.read(request_file)
             request = Request.new(request_string)
-            _(request.method).must_equal :post
+            _(request.method).must_equal :get
         end
 
         it 'parses the resource' do
             request_string = File.read(request_file)
             request = Request.new(request_string)
-            _(request.resource).must_equal "/login"
+            _(request.resource).must_equal "/examples"
         end
-
+        
         it 'parses the version' do
             request_string = File.read(request_file)
             request = Request.new(request_string)
@@ -67,7 +67,10 @@ describe 'Request' do
         it 'parses the headers' do
             request_string = File.read(request_file)
             request = Request.new(request_string)
-            headers = {'Host' => 'foo.example', 'Content-Type' => 'application/x-www-form-urlencoded', 'Content-Length' => '39'}
+            headers = {'Host' => 'example.com',
+                 'User-Agent' => 'ExampleBrowser/1.0',
+                 'Accept-Encoding' => 'gzip, deflate',
+                 'Accept' => '*/*'}
             _(request.headers).must_equal headers
         end
 
@@ -79,6 +82,91 @@ describe 'Request' do
         end
 
     end
+    
+    describe 'Simple get-request' do
+    
+        request_file = './test/example_requests/get-fruits-with-filter.request.txt'
 
+        it 'parses the http method' do
+            request_string = File.read(request_file)
+            request = Request.new(request_string)
+            _(request.method).must_equal :get
+        end
+
+        it 'parses the resource' do
+            request_string = File.read(request_file)
+            request = Request.new(request_string)
+            _(request.resource).must_equal '/fruits?type=bananas&minrating=4'
+        end
+        
+        it 'parses the version' do
+            request_string = File.read(request_file)
+            request = Request.new(request_string)
+            _(request.version).must_equal 'HTTP/1.1'
+        end
+        
+        it 'parses the headers' do
+            request_string = File.read(request_file)
+            request = Request.new(request_string)
+            headers = {'Host' => 'fruits.com',
+                'User-Agent' => 'ExampleBrowser/1.0',
+                'Accept-Encoding' => 'gzip, deflate',
+                'Accept' => '*/*'}
+            _(request.headers).must_equal headers
+        end
+
+        it 'parses the headers' do
+            request_string = File.read(request_file)
+            request = Request.new(request_string)
+            params = {'type' => 'bananas', 'minrating' => '4'}
+            _(request.params).must_equal params
+        end
+
+    end
+
+    describe 'Simple post-request' do
+    
+        request_file = './test/example_requests/post-login.request.txt'
+
+        it 'parses the http method' do
+            request_string = File.read(request_file)
+            request = Request.new(request_string)
+            _(request.method).must_equal :post
+        end
+
+
+
+        it 'parses the resource' do
+            request_string = File.read(request_file)
+            request = Request.new(request_string)
+            _(request.resource).must_equal "/login"
+        end
+
+    
+
+        it 'parses the version' do
+            request_string = File.read(request_file)
+            request = Request.new(request_string)
+            _(request.version).must_equal 'HTTP/1.1'
+        end
+        
+
+
+        it 'parses the headers' do
+            request_string = File.read(request_file)
+            request = Request.new(request_string)
+            headers = {'Host' => 'foo.example', 'Content-Type' => 'application/x-www-form-urlencoded', 'Content-Length' => '39'}
+            _(request.headers).must_equal headers
+        end
+
+
+        it 'parses the headers' do
+            request_string = File.read(request_file)
+            request = Request.new(request_string)
+            params = {'username' => 'grillkorv', 'password' => 'verys3cret!'}
+            _(request.params).must_equal params
+        end
+
+    end
 
 end
