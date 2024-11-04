5e00a30db986a0d5dc2ee42f3fc0d68be4b9af55
SimonSuslich <simonniklas.suslich@elev.ga.ntig.se>
2024-11-04 10:17:03 +0100

    Dude i  got offenses

--- lib/request.rb
+++ lib/request.rb
@@ -1,17 +1,42 @@
-class Request
+# frozen_string_literal: true
 
+# Class that handles http requests
+class Request
   attr_reader :method, :resource, :version, :headers, :params
 
   def initialize(request_string)
-      request_lines = request_string.split("\n")
-      first_line, *rest_lines = request_lines 
-      @method, @resource, @version = first_line.split(" ")
-      @method = @method.downcase.to_sym
-      
-      @headers = rest_lines.map {|element| element.include?(":") ? element.split(":", 2).map(&:strip) : nil}.compact.to_h
-      
-      @params = {}
-      request_string[-2..-1] != "\n\n" ? @params = rest_lines[-1].split("&").map {|element| element.include?("=") ? element.split("=", 2).map(&:strip) : nil}.compact.to_h : nil
-      @resource.include?("?") ? @params = @resource.split("?", 2)[-1].split("&").map {|element| element.include?("=") ? element.split("=", 2).map(&:strip) : nil}.compact.to_h : nil
+    request_lines = request_string.split("\n")
+    first_line, *rest_lines = request_lines
+    create_methods(first_line)
+    create_headers(rest_lines)
+    create_params(request_string)
+  end
+
+  def create_methods(source_string)
+    @method, @resource, @version = source_string.split(' ')
+    @method = @method.downcase.to_sym
+  end
+
+  def create_headers(source_strings)
+    @headers = source_strings.select do |element|
+      element.include?(':')
+    end.map { |element| element.split(':', 2).map(&:strip) }.to_h
+  end
+
+  def create_params(source_string)
+    @params = {}
+
+    if source_string[-2..] != "\n\n"
+      @params = source_string.split("\n")[-1].split('&').map do |element|
+        element.include?('=') ? element.split('=', 2).map(&:strip) : nil
+      end.compact.to_h
+    end
+
+
+    return unless @resource.include?('?')
+
+    @params = @resource.split('?', 2)[-1].split('&').map do |element|
+      element.include?('=') ? element.split('=', 2).map(&:strip) : nil
+    end.compact.to_h
   end
 end
--- test/http_parser_spec.rb
+++ test/http_parser_spec.rb
@@ -1,172 +1,160 @@
+# frozen_string_literal: true
+
 require_relative 'spec_helper'
 require_relative '../lib/request'
 
 describe 'Request' do
+  describe 'Simple get-request' do
+    request_file = './test/example_requests/get-index.request.txt'
 
-    describe 'Simple get-request' do
-    
-        request_file = './test/example_requests/get-index.request.txt'
-
-        it 'parses the http method' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            _(request.method).must_equal :get
-        end
-
-        it 'parses the resource' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            _(request.resource).must_equal "/"
-        end
-        
-        it 'parses the version' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            _(request.version).must_equal 'HTTP/1.1'
-        end
-        
-        it 'parses the headers' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            headers = {'Host' => 'developer.mozilla.org', 'Accept-Language' => 'fr'}
-            _(request.headers).must_equal headers
-        end
-
-        it 'parses the headers' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            params = {}
-            _(request.params).must_equal params
-        end
-
-
+    it 'parses the http method' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      _(request.method).must_equal :get
     end
 
-    describe 'Simple get-request' do
-    
-        request_file = './test/example_requests/get-examples.request.txt'
-
-        it 'parses the http method' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            _(request.method).must_equal :get
-        end
-
-        it 'parses the resource' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            _(request.resource).must_equal "/examples"
-        end
-        
-        it 'parses the version' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            _(request.version).must_equal 'HTTP/1.1'
-        end
-        
-        it 'parses the headers' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            headers = {'Host' => 'example.com',
-                 'User-Agent' => 'ExampleBrowser/1.0',
-                 'Accept-Encoding' => 'gzip, deflate',
-                 'Accept' => '*/*'}
-            _(request.headers).must_equal headers
-        end
-
-        it 'parses the headers' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            params = {}
-            _(request.params).must_equal params
-        end
-
-    end
-    
-    describe 'Simple get-request' do
-    
-        request_file = './test/example_requests/get-fruits-with-filter.request.txt'
-
-        it 'parses the http method' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            _(request.method).must_equal :get
-        end
-
-        it 'parses the resource' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            _(request.resource).must_equal '/fruits?type=bananas&minrating=4'
-        end
-        
-        it 'parses the version' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            _(request.version).must_equal 'HTTP/1.1'
-        end
-        
-        it 'parses the headers' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            headers = {'Host' => 'fruits.com',
-                'User-Agent' => 'ExampleBrowser/1.0',
-                'Accept-Encoding' => 'gzip, deflate',
-                'Accept' => '*/*'}
-            _(request.headers).must_equal headers
-        end
-
-        it 'parses the headers' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            params = {'type' => 'bananas', 'minrating' => '4'}
-            _(request.params).must_equal params
-        end
-
+    it 'parses the resource' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      _(request.resource).must_equal '/'
     end
 
-    describe 'Simple post-request' do
-    
-        request_file = './test/example_requests/post-login.request.txt'
-
-        it 'parses the http method' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            _(request.method).must_equal :post
-        end
-
-
-
-        it 'parses the resource' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            _(request.resource).must_equal "/login"
-        end
-
-    
-
-        it 'parses the version' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            _(request.version).must_equal 'HTTP/1.1'
-        end
-        
-
-
-        it 'parses the headers' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            headers = {'Host' => 'foo.example', 'Content-Type' => 'application/x-www-form-urlencoded', 'Content-Length' => '39'}
-            _(request.headers).must_equal headers
-        end
-
-
-        it 'parses the headers' do
-            request_string = File.read(request_file)
-            request = Request.new(request_string)
-            params = {'username' => 'grillkorv', 'password' => 'verys3cret!'}
-            _(request.params).must_equal params
-        end
-
+    it 'parses the version' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      _(request.version).must_equal 'HTTP/1.1'
     end
 
+    it 'parses the headers' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      headers = { 'Host' => 'developer.mozilla.org', 'Accept-Language' => 'fr' }
+      _(request.headers).must_equal headers
+    end
+
+    it 'parses the headers' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      params = {}
+      _(request.params).must_equal params
+    end
+  end
+
+  describe 'Simple get-request' do
+    request_file = './test/example_requests/get-examples.request.txt'
+
+    it 'parses the http method' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      _(request.method).must_equal :get
+    end
+
+    it 'parses the resource' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      _(request.resource).must_equal '/examples'
+    end
+
+    it 'parses the version' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      _(request.version).must_equal 'HTTP/1.1'
+    end
+
+    it 'parses the headers' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      headers = { 'Host' => 'example.com',
+                  'User-Agent' => 'ExampleBrowser/1.0',
+                  'Accept-Encoding' => 'gzip, deflate',
+                  'Accept' => '*/*' }
+      _(request.headers).must_equal headers
+    end
+
+    it 'parses the headers' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      params = {}
+      _(request.params).must_equal params
+    end
+  end
+
+  describe 'Simple get-request' do
+    request_file = './test/example_requests/get-fruits-with-filter.request.txt'
+
+    it 'parses the http method' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      _(request.method).must_equal :get
+    end
+
+    it 'parses the resource' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      _(request.resource).must_equal '/fruits?type=bananas&minrating=4'
+    end
+
+    it 'parses the version' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      _(request.version).must_equal 'HTTP/1.1'
+    end
+
+    it 'parses the headers' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      headers = { 'Host' => 'fruits.com',
+                  'User-Agent' => 'ExampleBrowser/1.0',
+                  'Accept-Encoding' => 'gzip, deflate',
+                  'Accept' => '*/*' }
+      _(request.headers).must_equal headers
+    end
+
+    it 'parses the headers' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      params = { 'type' => 'bananas', 'minrating' => '4' }
+      _(request.params).must_equal params
+    end
+  end
+
+  describe 'Simple post-request' do
+    request_file = './test/example_requests/post-login.request.txt'
+
+    it 'parses the http method' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      _(request.method).must_equal :post
+    end
+
+    it 'parses the resource' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      _(request.resource).must_equal '/login'
+    end
+
+
+
+    it 'parses the version' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      _(request.version).must_equal 'HTTP/1.1'
+    end
+
+
+    it 'parses the headers' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      headers = { 'Host' => 'foo.example', 'Content-Type' => 'application/x-www-form-urlencoded',
+                  'Content-Length' => '39' }
+      _(request.headers).must_equal headers
+    end
+
+    it 'parses the headers' do
+      request_string = File.read(request_file)
+      request = Request.new(request_string)
+      params = { 'username' => 'grillkorv', 'password' => 'verys3cret!' }
+      _(request.params).must_equal params
+    end
+  end
 end
